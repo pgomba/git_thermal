e0a1f5b82560f0d75edf4e7757b4dca083c3ed29
pgomba <95383762+pgomba@users.noreply.github.com>
2022-03-02 12:26:23 +0000

    initial upload

---
+++ app.R
@@ -0,0 +1,62 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(readxl)
+library(tidyverse)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+  titlePanel("Thermal plate app"),
+  sidebarPanel(
+    fileInput("file", "Upload data*", accept = c(".xlsx")),
+    p("Upload .xlsx file. Other formats to be included in due course", style = "font-family: 'times'; font-si16pt")
+  ),
+  
+  mainPanel(
+    p("Thermal plate Petri dish labels scheme.", style = "font-family: 'times'; font-si16pt"),
+    img(src="thermal.png"),
+    br(),
+    br(),
+    p(HTML(paste0("Upload information following this template. Respect column names. Leave cell in blank if T",tags$sub("50")," is not known")), style = "font-family: 'times'; font-si16pt"),
+    img(src="thermal2.png"),
+    tableOutput("table"),
+    plotOutput("plot")
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$table<-renderTable({
+    file_to_read<-input$file
+    if(is.null(file_to_read)){
+      return()
+    }
+    read_xlsx(file_to_read$datapath)%>%head()
+  })
+  
+  bb<-reactive({
+    file_to_plot<-input$file
+    if(is.null(file_to_plot)){
+      return()
+    }
+    bb<-read_xlsx(file_to_plot$datapath)
+  })
+  
+  output$plot<-renderPlot({
+    ggplot(bb(),aes(x=a,y=b))+
+      geom_point()
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
